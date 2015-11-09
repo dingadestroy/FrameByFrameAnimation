eb88c3b87d02e96b4d633d628d4f2ec7beba619a
dingadestroy <rformusoh1@gmail.com>
2015-11-08 20:56:02 -0500

    2nd commit

--- app/src/main/java/com/example/ronald/framebyframeanimation/MainActivity.java
+++ app/src/main/java/com/example/ronald/framebyframeanimation/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ronald.framebyframeanimation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+        iv.setBackgroundResource(R.drawable.animation);
+        iv.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                AnimationDrawable anim = (AnimationDrawable) iv.getBackground();
+                anim.start();
+
+            }
+        });
+
+
     }
 }
